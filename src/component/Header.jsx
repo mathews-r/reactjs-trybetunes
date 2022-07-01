c580460284e7aeb51275374c7e54c0a396d22650
Mathews <mathewsrodrigues9@gmail.com>
2022-07-01 19:47:38 -0300

    req 3 ok

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: false,
+      user: '',
+    };
+  }
+
+  componentDidMount() {
+    this.waitAPI();
+  }
+
+  waitAPI = async () => {
+    this.setState({ isLoading: true });
+    const user = await getUser();
+    this.setState({ isLoading: false });
+    this.setState({ user: user.name });
+  }
+
   render() {
-    const userName = localStorage.getItem('user');
-    console.log(userName);
+    const { isLoading, user } = this.state;
     return (
-      <div>
-        {userName[0]}
+      <div data-testid="header-component">
+        { isLoading ? <Loading /> : <h2 data-testid="header-user-name">{user}</h2> }
       </div>
     );
   }
